fe82b456f8de69c157db53943cd561f42c2ea850
HairiRaven <152843094+DarkBytesx@users.noreply.github.com>
2025-02-06 07:46:44 +0800

    Update server.lua
    lil update

--- server.lua
+++ server.lua
@@ -11,18 +11,31 @@ local function getPlayerIdentifier(playerId)
     return nil
 end
 
-RegisterNetEvent('billing:createBill')
-AddEventHandler('billing:createBill', function(playerId, amount, reason, comment)
+function getPlayerIdFromLicense(license)
+    for _, playerId in ipairs(GetPlayers()) do 
+        local identifiers = GetPlayerIdentifiers(playerId) 
+        for _, id in ipairs(identifiers) do
+            if id == license then
+                return playerId 
+            end
+        end
+    end
+    return nil  
+end
+
+
+
+RegisterNetEvent('d-invoices:createBill')
+AddEventHandler('d-invoices:createBill', function(playerId, amount, reason, comment)
     local src = source
     local playerName = GetPlayerName(src)
     local identifier = getPlayerIdentifier(playerId)
-
     local xPlayer = ESX.GetPlayerFromId(src)
     local playerJob = xPlayer.job.name 
-
+    local receiverName = GetPlayerName(playerId)
     if identifier then
-        exports.oxmysql:insert("INSERT INTO billing (sender, receiver, amount, reason, comment, status, organization, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())", {
-            playerName, identifier, amount, reason, comment, 'Unpaid', playerJob 
+        exports.oxmysql:insert("INSERT INTO billing (sender, receiver, receiver_name, amount, reason, comment, status, organization, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())", {
+            playerName, identifier, receiverName, amount, reason, comment, 'Unpaid', playerJob 
         }, function(id)
             if id then
                 exports.oxmysql:execute("SELECT id, amount, status, created_at, organization FROM billing WHERE receiver = ? AND status = 'Unpaid'", {identifier}, function(bills)
@@ -36,6 +49,14 @@ AddEventHandler('billing:createBill', function(playerId, amount, reason, comment
                     description = locale('bill_created'),
                     type = "success"
                 })
+                TriggerClientEvent('ox_lib:notify', playerId, {
+                    title = locale('title'),
+                    description = locale("you_have_been_fined"),
+                    type = "info"
+                })
+                local message = string.format("**Bill Created**\n**Sender:** %s\n**Receiver:** %s\n**Reason:** %s\n**Amount:** %s\n**Comment:** %s", playerName, receiverName, reason, amount, comment)
+                SendToDiscord(Config.DiscordWebhook.Invoices, 'Bill created', message, 65280) 
+
             else
                 TriggerClientEvent('ox_lib:notify', src, {
                     title = locale('title'),
@@ -53,8 +74,8 @@ AddEventHandler('billing:createBill', function(playerId, amount, reason, comment
     end
 end)
 
-RegisterNetEvent('billing:fetchDashboardInfo')
-AddEventHandler('billing:fetchDashboardInfo', function()
+RegisterNetEvent('d-invoices:fetchDashboardInfo')
+AddEventHandler('d-invoices:fetchDashboardInfo', function()
     local src = source
     local identifier = getPlayerIdentifier(src)  
 
@@ -68,7 +89,7 @@ AddEventHandler('billing:fetchDashboardInfo', function()
                 ox:execute("SELECT COUNT(*) AS unpaidBills FROM billing WHERE receiver = ? AND status = 'Unpaid'", {identifier}, function(unpaidResult)
                     local unpaidBills = unpaidResult[1].unpaidBills or 0
 
-                    TriggerClientEvent('billing:receiveDashboardInfo', src, {
+                    TriggerClientEvent('d-invoices:client:receiveDashboardInfo', src, {
                         totalBills = totalBills,
                         paidBills = paidBills,
                         unpaidBills = unpaidBills
@@ -79,8 +100,8 @@ AddEventHandler('billing:fetchDashboardInfo', function()
     end
 end)
 
-RegisterNetEvent('billing:fetchBills')
-AddEventHandler('billing:fetchBills', function()
+RegisterNetEvent('d-invoices:fetchBills')
+AddEventHandler('d-invoices:fetchBills', function()
     local src = source
     local identifier = getPlayerIdentifier(src)
 
@@ -91,8 +112,8 @@ AddEventHandler('billing:fetchBills', function()
     end
 end)
 
-RegisterNetEvent('billing:fetchHistory')
-AddEventHandler('billing:fetchHistory', function()
+RegisterNetEvent('d-invoices:fetchHistory')
+AddEventHandler('d-invoices:fetchHistory', function()
     local src = source
     local identifier = getPlayerIdentifier(src)
 
@@ -105,23 +126,43 @@ AddEventHandler('billing:fetchHistory', function()
     end
 end)
 
-
-RegisterNetEvent('billing:payBill')
-AddEventHandler('billing:payBill', function(billId)
+RegisterNetEvent('d-invoices:payBill')
+AddEventHandler('d-invoices:payBill', function(billId)
     local src = source
     local identifier = getPlayerIdentifier(src)
-    
+
     ox:execute("SELECT amount, organization FROM billing WHERE id = ? AND receiver = ?", {billId, identifier}, function(result)
         if result and #result > 0 then
             local amount = result[1].amount
+            local organization = result[1].organization
             local xPlayer = ESX.GetPlayerFromId(src)
-            local money = xPlayer.getMoney() 
+            local money = Config.UseBank and xPlayer.getAccount('bank').money or xPlayer.getMoney()
 
             if money >= amount then
-                xPlayer.removeMoney(amount)
+                if Config.UseBank then
+                    xPlayer.removeAccountMoney('bank', amount)
+                else
+                    xPlayer.removeMoney(amount)
+                end
+
+                local societyAccount = nil
+                for _, job in pairs(Config.AllowedJobs) do
+                    if job.job == organization then
+                        societyAccount = job.society
+                        break
+                    end
+                end
+
+                if societyAccount then
+                    TriggerEvent('esx_addonaccount:getSharedAccount', societyAccount, function(account)
+                        if account then
+                            account.addMoney(amount) -- Add money to the society account
+                        end
+                    end)
+                end
+
                 ox:execute("UPDATE billing SET status = 'Paid' WHERE id = ?", {billId}, function(affectedRows)
                     if affectedRows then
-
                         ox:execute("SELECT id, amount, status, created_at FROM billing WHERE receiver = ? AND status = 'Unpaid'", {identifier}, function(bills)
                             TriggerClientEvent('d-invoices:client:receiveBills', src, bills)
                         end)
@@ -135,6 +176,9 @@ AddEventHandler('billing:payBill', function(billId)
                             description = locale('paid'),
                             type = "success"
                         })
+                        local playerName = GetPlayerName(src)
+                        local message = string.format("\n**Paid by:** %s\n**Amount:** %s\n**Sent to:** %s", playerName, amount, societyAccount)
+                        SendToDiscord(Config.DiscordWebhook.Invoices, 'Bill paid', message, 65280)
                     else
                         TriggerClientEvent('ox_lib:notify', src, {
                             title = locale('title'),
@@ -159,3 +203,82 @@ AddEventHandler('billing:payBill', function(billId)
         end
     end)
 end)
+
+RegisterNetEvent('d-invoices:cancelInvoice')
+AddEventHandler('d-invoices:cancelInvoice', function(invoiceId, amount, reason, comment)
+    local src = source
+    MySQL.update('UPDATE billing SET status = "Paid" WHERE id = ?', {invoiceId}, function(affectedRows)
+        if affectedRows > 0 then
+            local invoices = MySQL.query.await('SELECT * FROM billing WHERE id = ?', {invoiceId})
+
+            if #invoices > 0 then
+                local invoice = invoices[1]
+                local receiverName = invoice.receiver_name
+                local sender = invoice.sender
+                local reason = invoice.reason
+                local amount = invoice.amount
+                local comment = invoice.comment
+                local organization = invoice.organization
+
+                local message = string.format("**Sender:** %s\n**Receiver:** %s\n**Reason:** %s\n**Amount:** %s\n**Comment:** %s\n**Organization:** %s", 
+                    sender, receiverName, reason, amount, comment, organization)
+
+                SendToDiscord(Config.DiscordWebhook.Invoices, 'Bill cancelled', message, 65280)
+                TriggerClientEvent('ox_lib:notify', src, {
+                    title = locale('title'),
+                    description = locale("invoice_canceled"),
+                    type = "info"
+                })
+            else
+                TriggerClientEvent('ox_lib:notify', src, {
+                    title = locale('title'),
+                    description = locale("invoice_not_found"),
+                    type = "info"
+                })
+            end
+        else
+            TriggerClientEvent('ox_lib:notify', src, {
+                title = locale('title'),
+                description = locale("failed_to_update"),
+                type = "info"
+            })
+        end
+    end)
+end)
+
+
+lib.callback.register('d-invoices:getAllPlayerData', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if not xPlayer then return { error = 'Player not found' } end
+
+    local playerJob = xPlayer.job.name
+    local invoices = MySQL.query.await("SELECT * FROM billing WHERE status = 'Unpaid' AND organization = ?", {playerJob})
+
+    for i = 1, #invoices do
+        local receiverName = invoices[i].receiver_name 
+        invoices[i].receiver = receiverName  
+    end
+
+    return invoices  
+end)
+
+
+
+
+function SendToDiscord(webhook, title, message, color)
+    local embed = {
+        {
+            ["title"] = title,
+            ["description"] = message,
+            ["color"] = color, 
+            ["footer"] = {
+                ["text"] = os.date("%Y-%m-%d %H:%M:%S"), 
+            },
+        }
+    }
+
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({
+        username = "Djonza Invoices", 
+        embeds = embed
+    }), { ['Content-Type'] = 'application/json' })
+end
